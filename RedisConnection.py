2d0e2fccb4f672b3e7c1123bd35473c982406dc6
gilz <gilz@codevalue.net>
2019-06-30 19:13:55 +0300

    fix

--- RedisConnection.py
+++ RedisConnection.py
@@ -4,7 +4,7 @@ import redis
 def connect():
     global redisCon
     if redisCon is None:
-        host = os.getenv("REDIS_HOST")
+        host = os.getenv("REDIS_HOST").split(":")
         redisCon = redis.StrictRedis(host=host[0], port=int(host[1]))
 
     return redisCon
