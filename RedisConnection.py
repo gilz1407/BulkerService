4c13cbaf65907f79b251e526d9105a2b4f79eafc
Ido braunstain <idob@172-10-71-30.lightspeed.jcsnms.sbcglobal.net>
2019-06-30 21:23:06 +0300

    dockerize

--- RedisConnection.py
+++ RedisConnection.py
@@ -6,6 +6,6 @@ def connect():
     global redisCon
     if redisCon is None:
         host = os.getenv("REDIS_HOST").split(":")
-        redisCon = redis.StrictRedis(host=host[0], port=int(host[1]))
+        redisCon = redis.StrictRedis(host=host[0], port=int(host[-1]))
 
     return redisCon
