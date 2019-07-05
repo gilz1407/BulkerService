547a97f5473832c4ba3fcaccf67b65f4f56b666e
gilz <gilz@codevalue.net>
2019-07-05 20:14:05 +0300

    fix

--- BulkerTet.py
+++ BulkerTet.py
@@ -5,7 +5,7 @@ def streamBars():
     close = 1
     min = 1
     max = 1
-    while open<37:
+    while open<50:
         requests.post('http://127.0.0.1:7000/Bulker/AddBar',json={"open":open,"close":close,"min":min,"max":max})
         open = open + 1
         close = close + 1
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import configparser
 import json
 import os
-
 from flask import Flask, request
 from RedisConnection import connect
 
@@ -12,6 +11,7 @@ redisCheckThread = None
 stack = []
 forPublish = {}
 minLength = 0
+startPoint = 0
 r = connect()
 @app.route('/Bulker/AddBar',methods=['POST'])
 def AddBar():
@@ -22,6 +22,16 @@ def AddBar():
     return ""
 
 def GenerateBulks():
+    if len(stack) >= lst[0][1]:
+        for idx in range(lst[0][1], lst[-1][1]+1):
+            if (len(stack)-idx) >= 0:
+                forPublish["Bars"] = stack[len(stack)-idx:len(stack)]
+                r.lpush(configDef['publishOn'], json.dumps(forPublish))
+            else:
+                break
+
+
+def GenerateBulksOLD():
     global stack, r, minLength
     forPublish["from"] = 0
     stackLength = len(stack)
@@ -34,11 +44,13 @@ def GenerateBulks():
         print("Delete: "+str(len(stack)))
         del stack[0:1]
         print("Length of the stack after delete: " + str(stackLength) + "\n")
-        minLength = minLength - lst[0][1]
-    if stackLength >= minLength:
+        minLength = 0
+    if len(stack) >= minLength:
         for index in range(minLength, stackLength+1):
+            if index<0:
+                pass
             forPublish["To"] = index
-            forPublish["Bars"] = stack[0:stackLength+1]
+            forPublish["Bars"] = stack[0:index+1]
             print("From: " + str(stack[0]) + " To: " + str(index))
             r.lpush(configDef['publishOn'], json.dumps(forPublish))
         minLength = minLength + 1
@@ -53,5 +65,6 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.ini')
     configDef = config['DEFAULT']
+    r.delete(configDef['publishOn'])
     app.config['SERVER_NAME'] = os.getenv("Bulker_HOST")
-    app.run(debug=True)
+    app.run(debug=False)
